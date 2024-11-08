97d71aa3777cb756927137daaa389504de7036e1
Maxus <153613487+maxu-s@users.noreply.github.com>
2024-11-08 22:05:24 +0100

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
-    path('gamemodes/', views.gamemodes, name='gamemodes'),
-    path('ranking/', views.ranking, name='ranking'),
+    path('about/', views.about, name='about'),
+    path('download/', views.download, name='download'),
     path('servers/', views.servers, name='servers'),
     path('tutorials/', views.tutorials, name='tutorials'),
 ]
